26f904eb6513d91e12d618a050b3f5422d40c23b
Yury Matuhin <ym@ymatuhin.ru>
2017-04-08 14:08:15 +0300

    Upload metrica errors cathc

---
+++ metrica_errors.js
@@ -0,0 +1,45 @@
+"use strict";
+
+var getMetrica = function getMetrica() {
+    var metrica = null;
+    for (key in window) {
+        if (key.indexOf("yaCounter") === 0) {
+            metrica = window[key];
+        }
+    }
+    if (!metrica) throw new Error("Yandex Metrica not found!");
+    return metrica;
+};
+
+var sendMessage = function sendMessage(message) {
+    var metrica = getMetrica();
+    metrica.hit(location.pathname, message);
+};
+
+var browserErrorHandler = function browserErrorHandler(errorMsg, url, lineNumber, column, errorObj) {
+    var message = {
+        "JavaScript errors": {
+            where: url + " " + lineNumber + ":" + column,
+            stacktrace: errorObj
+        }
+    };
+    sendMessage(message);
+};
+
+var browserRejectionHandler = function browserRejectionHandler(event) {
+    var message = {
+        "JavaScript unhandled rejection": event
+    };
+    sendMessage(message);
+};
+
+var listeners = function listeners() {
+    if (window.addEventListener) {
+        window.addEventListener("error", browserErrorHandler);
+        window.addEventListener("unhandledrejection", browserRejectionHandler);
+    } else {
+        window.onerror = browserErrorHandler;
+    }
+};
+
+listeners();
