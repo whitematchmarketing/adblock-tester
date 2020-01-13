4185195477f63403232accae2f915844dd9e1922
ymatuhin <ym@ymatuhin.ru>
2020-01-13 03:51:43 +0300

    Fix image local loading check, updated some texts

--- src/utils/fetchMediaFileSize.ts
+++ src/utils/fetchMediaFileSize.ts
@@ -1,6 +1,17 @@
+const allowedTypes = ["application/x-shockwave-flash", "image"];
+
 export const fetchMediaFileSize = (url: string) =>
   fetch(url)
-    .then(req => req.text())
+    .then(res => {
+      const contentType = res.headers.get("content-type");
+      const acceptableType = allowedTypes.reduce(
+        (acc, type) => (contentType.indexOf(type) === 0 ? true : acc),
+        false,
+      );
+      if (!acceptableType) throw Error("Wrong type response");
+      return res;
+    })
+    .then(res => res.text())
     .then(text => text.length)
     .then(fileSize => {
       if (fileSize > 0) return fileSize;
