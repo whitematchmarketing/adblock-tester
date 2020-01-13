564dbe50be2931d42df49c7833be9e082e6fd765
ymatuhin <ym@ymatuhin.ru>
2020-01-13 04:34:33 +0300

    Fix wrong status code for media requests

--- src/utils/fetchMediaFileSize.ts
+++ src/utils/fetchMediaFileSize.ts
@@ -3,11 +3,13 @@ const allowedTypes = ["application/x-shockwave-flash", "image"];
 export const fetchMediaFileSize = (url: string) =>
   fetch(url)
     .then(res => {
+      const isOk = res.status >= 200 && res.status < 300;
       const contentType = res.headers.get("content-type");
       const acceptableType = allowedTypes.reduce(
         (acc, type) => (contentType.indexOf(type) === 0 ? true : acc),
         false,
       );
+      if (!isOk) throw Error("Wrong status code");
       if (!acceptableType) throw Error("Wrong type response");
       return res;
     })
