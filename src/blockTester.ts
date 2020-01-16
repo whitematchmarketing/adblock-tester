cc88fe875aa4d746dc0829c38b0a7bfe7a0357a1
ymatuhin <ym@ymatuhin.ru>
2020-01-16 03:51:17 +0300

    Add debug logs

--- src/blockTester.ts
+++ src/blockTester.ts
@@ -2,15 +2,16 @@ import { t } from "./i18n";
 
 export const blockTester = () => {
   var script = document.createElement("script");
-  script.type = "text/javascript";
   script.innerHTML = `
     function isInvisible (elem) {
       return !(elem.offsetWidth || elem.offsetHeight || elem.getClientRects().length);
     };
     var showed = false;
     setTimeout(function timer() {
+      console.log("# timer start")
       if (showed) return;
       if (isInvisible(document.documentElement) || isInvisible(document.body) || isInvisible(document.getElementById("app"))) {
+        console.log("# invisible");
         showed = true;
         var result = prompt('${t("page.blockedMessage")}', '${t("page.blockedUrl")}');
         if (result) location.href = "${t("page.blockedUrl")}";
