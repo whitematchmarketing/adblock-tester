53711a1d45f265fbd8a713e5fb6f62e403c65b75
ymatuhin <ym@ymatuhin.ru>
2020-01-16 03:55:30 +0300

    Remove blockTester and try to inline html

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,8 @@ import "whatwg-fetch";
 import "@babel/polyfill";
 // @ts-ignore
 import App from "./app.svelte";
-import { blockTester } from "./blockTester";
 import "./model";
 
-blockTester();
 const app = new App({
   target: document.getElementById("app"),
   hydrate: process.env.NODE_ENV === "production",
