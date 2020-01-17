48dde44f2bea836b93b181d7d9a38d381b7a8d5f
ymatuhin <ym@ymatuhin.ru>
2020-01-17 05:59:42 +0300

    Small fixes

--- build.sh
+++ build.sh
@@ -11,15 +11,3 @@ echo "Build ru"
 NODE_ENV=production LANG=ru $PARCEL build ./src/index.html --out-dir ./checkadblock.ru --no-cache --detailed-report
 echo "Build en"
 NODE_ENV=production LANG=en $PARCEL build ./src/index.html --out-dir ./adblock-tester.com --no-cache --detailed-report
-
-echo "Check env cached in build"
-RUGREP=$(grep -q 'localhost:1234' ./checkadblock.ru/index.html)
-ENGREP=$(grep -q 'localhost:1234' ./adblock-tester.com/index.html)
-
-if [ "$ENGREP" != "" ] | [ "$RUGREP" != "" ]
-then
-  echo "String 'localhost:1234' was found in builds, that's parcel env error. You have to rebuild again and hope that everything will be ok next time."
-  exit 1;
-else
-  echo "Build was done successfully"
-fi
