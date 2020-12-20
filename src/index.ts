9e1b223526b7db6c1ae232e3b7073f67fda1d1fc
ymatuhin <ym@ymatuhin.ru>
2020-12-20 03:52:37 +0300

    Add warning and missing translation

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import "@babel/polyfill";
 import App from "./app.svelte";
 import "./model";
 
+if (!process.env.LANG) throw "process.env.LANG is undefined"
+
 const app = new App({
   target: document.getElementById("app"),
   hydrate: process.env.NODE_ENV === "production",
